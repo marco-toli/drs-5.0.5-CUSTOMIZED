33b959fa7a879c0310a3760ca01128f2f82e9a00
toli <cambusecritiche.ricette@gmail.com>
2018-02-06 19:40:26 +0100

    daisy chain

--- include/functions.h
+++ include/functions.h
@@ -27,12 +27,14 @@ float GetPedestal(float* vals, const unsigned int& s1, const unsigned int& s2);
 std::pair<std::pair<float,std::pair<float,float> >,float> GetAmplitudePulse(float* vals, const unsigned int& s1, const unsigned int& s2, const bool& isNegative);
 std::pair<std::pair<float,std::pair<float,float> >,float> GetAmplitudeIntegral(float* vals, const unsigned int& s1, const unsigned int& s2, const bool& isNegative);
 
-std::pair<float,float> GetAmplitudeSquare(float* vals, const unsigned int& s1, const unsigned int& s2, const unsigned int& sMin, const unsigned int& nS, const bool& isNegative);
+std::pair<float,float> GetAmplitudeSquare(float* vals, const unsigned int& s1, const unsigned int& s2, const float& SqTH, const unsigned int& nS, const bool& isNegative);
 
 std::pair<std::pair<float,float>,std::pair<TF1*,TF1*> > GetTimeTh(float* vals, const unsigned int& s1, const float& ped, const float& amp,
                                                                   const float& th, const unsigned int& nSBef, const unsigned int& nSAft,
                                                                   const bool& isFraction, const bool& isNegative, const bool& deleteFit = true);
 // std::pair<std::pair<float,float>,TF1*> GetTimeTemplateFit(float* vals, const TProfile* p_template, const float& ped, const bool& draw = false);
+
+std::vector <int> GetSpikes(float* vals, const float& spikeAmp);
 
 TGraph* PointsAroundTh(float* vals, const unsigned int& sBef, const unsigned int& nSBef, const unsigned int& nSAft);
 
